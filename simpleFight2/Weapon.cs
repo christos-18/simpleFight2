0dcbc2ac70701aae70acbf7990aa86e0f6cc1677
christos <potamitischristos@gmail.com>
2021-09-27 09:39:42 +0200

    1

--- simpleFight2/Weapon.cs
+++ simpleFight2/Weapon.cs
@@ -5,6 +5,11 @@ namespace simpleFight2
     public class Weapon
     {
         public string weapon;
+
+
+        Random generator = new Random();
+
+        int weaponDamage = generator.Next(10, 21);
         public Weapon()
         {
             Console.WriteLine("Välj vilket vapen du vill använda: \n \n Alla vapen är olika bra \n \n  weapon 1 = Glock 19 \n  weapon = Sig Sauer P365 \n  weapon 3 = Taurus G2c\n ");
